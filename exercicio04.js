26a0304e03383d981e790eaae9ec489f4f13edbc
aluno <e023aluno@outlook.com>
2026-04-15 22:09:59 -0300

    Final da aula

---
+++ exercicio04.js
@@ -0,0 +1,69 @@
+const prompt = require("prompt-sync")()
+
+
+ let numero = Number(prompt("Digite um número: "))
+
+    if(numero % 2 == 0){
+
+        
+
+        TabuadaCrescente()
+    
+    
+    } else {
+        TabuadaCrescente
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        console.log("\nTabuada Crescente (Número Ímpar)")
+        for(let i = 1; i <= 10; i++){
+            console.log(numero + " x " + i + " = " + (numero * i))
+        }
+    }
+
+
+
+
+   
+   
+   
+   
+   
+   
+ function TabuadaCrescente(){
+
+        console.log("\nTabuada Decrescente (Número Par)")
+        for(let i = 10; i >= 1; i--){
+            console.log(numero + " x " + i + " = " + (numero * i))
+        }
+
+    }
+
+
---
+++ funcao.js
@@ -0,0 +1,24 @@
+ function saudacao(){
+    console.log("Diga ola")
+
+    console.log ("diga tchau")
+ }
+ saudacao()
+
+ /************************************* */
+
+ 
+
+ function somar(a, b){ // parametros
+
+    return a + b;
+
+ }
+
+ console.log(somar(10, 20)) // argumentos
+
+ 
+    
+ 
+
+    
